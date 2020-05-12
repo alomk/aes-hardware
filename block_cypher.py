f404960fafd93c8309ed0e1255799d39fa13a7af
Kazi <alomk@mit.edu>
2020-05-12 00:22:45 -0400

    working python interface

--- block_cypher.py
+++ block_cypher.py
@@ -1,9 +1,12 @@
 import serial
 import argparse
+import math
 import os
 import sys
 import binascii
+import time
 
+# two args: filepath and destination
 parser = argparse.ArgumentParser(description='Encrypt/decrypt using the PSoC 5LP')
 parser.add_argument('filepath', action="store")
 parser.add_argument('destination', action="store")
@@ -15,27 +18,30 @@ if (not os.path.isfile(args.filepath)):
     print("Enter a valid filepath")
     exit()
 
-ser = serial.Serial('/dev/ttyUSB0', timeout=1)
+# open serial connection
+ser = serial.Serial('/dev/ttyUSB1', timeout=1)
+ser.read_all() # flush output
 
+# input bytes converted to a hex string
 fp = binascii.hexlify(open(args.filepath,'rb').read())
 
-ret = []
+# fp is now padded to a multiple of 32
+if (len(fp) % 32 != 0):
+    fp += (32-(len(fp) % 32)) * b'0'
 
 i = 0
-while (i < round(len(fp)/16)):
-    ser.write(fp[i*16:i*16 + 16])
+while (i < len(fp)/32):
+    tmp = fp[i*32:i*32 + 32] 
+    ser.write(tmp) # write chunk to serial
+    time.sleep(1)
     i += 1
 
-remainder = len(fp) - i*16
-ser.write(fp[-remainder:] + b'\x00'*(16 - remainder))
 
-lines = ser.readlines()
-import ipdb; ipdb.set_trace()
-ser.close()
+buf = ser.readlines()[0]
 
 output = open(args.destination,"wb")
-for line in lines:
-    output.write(binascii.unhexlify(line))
+output.write(binascii.unhexlify(buf))
 
 output.close()
+ser.close()
 
